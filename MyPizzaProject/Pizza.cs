f400e56d5a09acfe3708a9ab06d29c1d5db009e3
nicomp42 <nicholdw@ucmail.uc.edu>
2024-03-21 10:07:37 -0400

    Add project files.

---
+++ MyPizzaProject/Pizza.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPizzaProject
+{
+    /// <summary>
+    /// Model a pizza that's been ordered at LaRosa's
+    /// </summary>
+    internal class Pizza
+    {
+        public enum size {Undefined, Small, Medium, Large, X_Large };
+        private size pizzaSize;
+        private ArrayList toppings;
+        /// <summary>
+        /// The size of the pizza
+        /// </summary>
+        size Size {
+            get { return  pizzaSize; }
+            set { pizzaSize = value; }
+        }
+        /// <summary>
+        /// Initialize a Pizza
+        /// </summary>
+        public Pizza()
+        {
+            // Create an empty list of toppings for this new Pizza object
+            toppings = new ArrayList();
+            Size = size.Undefined;
+        }
+        /// <summary>
+        /// Bake the pizza 
+        /// </summary>
+        public void Bake()
+        {
+            Console.WriteLine("Baking the pizza...");
+        }
+        /// <summary>
+        /// Add a topping to the Pizza
+        /// </summary>
+        /// <param name="topping">The topping to be added</param>
+        public void AddTopping(String topping)
+        {
+            toppings.Add(topping);
+        }
+        public override String ToString()
+        {
+            return "Size: " + Size.ToString() + "Toppings: " + toppings.ToString();
+        }
+    }
+}
---
+++ MyPizzaProject/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPizzaProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
